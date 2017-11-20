a447dc7779249477908f8cb0ddf700cb9c1f1c6c
Cliff L. Biffle <code@cliffle.com>
2017-11-19 21:17:21 -0800

    Rework test suite, centralize boilerplate.

--- rtl/test/CFMSpec.hs
+++ rtl/test/CFMSpec.hs
@@ -60,127 +60,109 @@ spec = do
 
     it "loads T" $ property $ \(Load s) v ->
       go s v ^. _2 . msT == v
+  context "universal instruction properties" $ do
+    let go s x = cycle' s (IS x u u)
+        u = errorX "must not be used in this test"
+
+    it "addresses R using new RPtr" $ property $ \x (Fetch s) ->
+      let (o, s') = go s x
+      in o ^. osROp . _1 == s' ^. msRPtr
+    it "addresses D using new DPtr" $ property $ \x (Fetch s) ->
+      let (o, s') = go s x
+      in o ^. osDOp . _1 == s' ^. msDPtr
 
   context "literal push" $ do
     let mklit :: BitVector 15 -> BitVector 16
         mklit x = 1 ++# x
         go s x = cycle' s (IS (mklit x) u u)
         u = errorX "must not be used in this test"
-        test p l = \x (Fetch s) -> p $ go s x ^. l
-    -- Black-box (outputs-based) properties
-    it "doesn't write Memory" $ property $ test isNothing (_1 . osMWrite)
-    it "doesn't write Return" $ property $ test isNothing (_1 . osROp . _2)
+
+    instDoesNotWriteM mklit
+    instDoesNotWriteR mklit
+    instPreservesRPtr mklit
+    instLeavesLFClear mklit
+    instFetches mklit
+    it "increments DPtr" $ instChanges mklit msDPtr (+1)
+    it "advances PC" $ instChanges mklit msPC (+1)
 
     it "flushes T" $ property $ \x (Fetch s) ->
       go s x ^. _1 . osDOp == (s ^. msDPtr + 1, Just (s ^. msT))
 
-    -- White-box (microarchitectural) properties
-    let stdelta f l = \x (Fetch s) -> (go s x ^. _2 . l) == f (s ^. l)
-
-    it "doesn't set LF" $ property $ stdelta id msLoadFlag
-    it "increments DPtr" $ property $ stdelta (+1) msDPtr
-    it "preserves RPtr" $ property $ stdelta id msRPtr
-    it "advances PC" $ property $ stdelta (+1) msPC
-    it "loads T" $ property $ \x (Fetch s) ->
+    it "loads T from literal" $ property $ \x (Fetch s) ->
       go s x ^. _2 . msT == zeroExtend x
-    it "fetches next" $ property $ \x (Fetch s) ->
-      go s x ^. _1 . osMRead == Just (s ^. msPC + 1)
-    it "addresses D" $ property $ \x (Fetch s) ->
-      go s x ^. _1 . osDOp . _1 == s ^. msDPtr + 1
-    it "addresses R" $ property $ \x (Fetch s) ->
-      go s x ^. _1 . osROp . _1 == s ^. msRPtr
 
   context "jump" $ do
     let mkjmp :: BitVector 13 -> BitVector 16
         mkjmp x = 0 ++# x
-        go s x = cycle' s (IS (mkjmp x) u u)
+
+    instDoesNotWriteM mkjmp
+    instDoesNotWriteR mkjmp
+    instDoesNotWriteD mkjmp
+    instLeavesLFClear mkjmp
+    instPreservesDPtr mkjmp
+    instPreservesRPtr mkjmp
+    instFetches mkjmp
+
+    it "preserves T" $ instPreserves mkjmp msT
+
+    let go s x = cycle' s (IS (mkjmp x) u u)
         u = errorX "must not be used in this test"
-        test p l = \x (Fetch s) -> p $ go s x ^. l
-    -- Black-box (outputs-based) properties
-    it "doesn't write Memory" $ property $ test isNothing (_1 . osMWrite)
-    it "doesn't write Return" $ property $ test isNothing (_1 . osROp . _2)
-    it "doesn't write Data" $ property $ test isNothing (_1 . osDOp . _2)
 
-    -- White-box (microarchitectural) properties
-    let stdelta f l = \x (Fetch s) -> (go s x ^. _2 . l) == f (s ^. l)
-
-    it "doesn't set LF" $ property $ stdelta (const False) msLoadFlag
-    it "preserves DPtr" $ property $ stdelta id msDPtr
-    it "preserves RPtr" $ property $ stdelta id msRPtr
-    it "preserves T" $ property $ stdelta id msT
-    it "advances PC" $ property $ \x (Fetch s) ->
+    it "always jumps" $ property $ \x (Fetch s) ->
       go s x ^. _2 . msPC == zeroExtend x
-    it "fetches next" $ property $ \x (Fetch s) ->
-      go s x ^. _1 . osMRead == Just (zeroExtend x)
-    it "addresses D" $ property $ \x (Fetch s) ->
-      go s x ^. _1 . osDOp . _1 == s ^. msDPtr
-    it "addresses R" $ property $ \x (Fetch s) ->
-      go s x ^. _1 . osROp . _1 == s ^. msRPtr
 
   context "conditional jump" $ do
     let mkjmp :: BitVector 13 -> BitVector 16
         mkjmp x = 0b001 ++# x
-        go s x d = cycle' s (IS (mkjmp x) d u)
+    instDoesNotWriteM mkjmp
+    instDoesNotWriteR mkjmp
+    instDoesNotWriteD mkjmp
+    instLeavesLFClear mkjmp
+    instPreservesRPtr mkjmp
+    instFetches mkjmp
+
+    it "decrements DPtr" $ instChanges mkjmp msDPtr (+ 0xFFFF)
+
+    let go s x d = cycle' s (IS (mkjmp x) d u)
         u = errorX "must not be used in this test"
-        test p l = \x (Fetch s) d -> p $ go s x d ^. l
-    -- Black-box (outputs-based) properties
-    it "doesn't write Memory" $ property $ test isNothing (_1 . osMWrite)
-    it "doesn't write Return" $ property $ test isNothing (_1 . osROp . _2)
-    it "doesn't write Data" $ property $ test isNothing (_1 . osDOp . _2)
+        stdelta f l = \x (Fetch s) d -> (go s x d ^. _2 . l) == f (s ^. l)
 
-    -- White-box (microarchitectural) properties
-    let stdelta f l = \x (Fetch s) d -> (go s x d ^. _2 . l) == f (s ^. l)
-
-    it "doesn't set LF" $ property $ stdelta id msLoadFlag
-    it "decrements DPtr" $ property $ stdelta (+ 0xFFFF) msDPtr
-    it "preserves RPtr" $ property $ stdelta id msRPtr
-    it "pops T" $ property $ \x (Fetch s) d ->
+    it "loads T from N" $ property $ \x (Fetch s) d ->
       go s x d ^. _2 . msT == d
       
-    it "advances PC" $ property $ \x (Fetch s) d ->
+    it "jumps when T==0, otherwise proceeds" $ property $ \x (Fetch s) d ->
       go s x d ^. _2 . msPC == if s ^. msT == 0
                                  then zeroExtend x
                                  else s ^. msPC + 1
-    it "fetches next" $ property $ \x (Fetch s) d ->
-      go s x d ^. _1 . osMRead == if s ^. msT == 0
-                                    then Just (zeroExtend x)
-                                    else Just (s ^. msPC + 1)
-    it "addresses D" $ property $ \x (Fetch s) d ->
-      go s x d ^. _1 . osDOp . _1 == s ^. msDPtr - 1
-    it "addresses R" $ property $ \x (Fetch s) d ->
-      go s x d ^. _1 . osROp . _1 == s ^. msRPtr
 
   context "call" $ do
     let mkcall :: BitVector 13 -> BitVector 16
         mkcall x = 0b010 ++# x
-        go s x = cycle' s (IS (mkcall x) u u)
+    instDoesNotWriteM mkcall
+    instDoesNotWriteD mkcall
+    instLeavesLFClear mkcall
+    instPreservesDPtr mkcall
+    instFetches mkcall
+
+    it "increments RPtr" $ instChanges mkcall msRPtr (+1)
+    it "preserves T" $ instPreserves mkcall msT
+
+    let go s x = cycle' s (IS (mkcall x) u u)
         u = errorX "must not be used in this test"
         test p l = \x (Fetch s) -> p $ go s x ^. l
-    -- Black-box (outputs-based) properties
-    it "doesn't write Memory" $ property $ test isNothing (_1 . osMWrite)
-    it "doesn't write Data" $ property $ test isNothing (_1 . osDOp . _2)
+        stdelta f l = \x (Fetch s) -> (go s x ^. _2 . l) == f (s ^. l)
 
-    it "pushes PC to R" $ property $ \x (Fetch s) ->
-      go s x ^. _1 . osROp == (s ^. msRPtr + 1, Just (s ^. msPC + 1))
+    it "pushes return PC to R" $ property $ \x (Fetch s) ->
+      go s x ^. _1 . osROp . _2 == Just (s ^. msPC + 1)
 
-    -- White-box (microarchitectural) properties
-    let stdelta f l = \x (Fetch s) -> (go s x ^. _2 . l) == f (s ^. l)
-
-    it "doesn't set LF" $ property $ stdelta id msLoadFlag
-    it "preserves DPtr" $ property $ stdelta id msDPtr
-    it "increments RPtr" $ property $ stdelta (+1) msRPtr
-    it "preserves T" $ property $ stdelta id msT
-    it "advances PC" $ property $ \x (Fetch s) ->
+    it "always jumps" $ property $ \x (Fetch s) ->
       go s x ^. _2 . msPC == zeroExtend x
-    it "fetches next" $ property $ \x (Fetch s) ->
-      go s x ^. _1 . osMRead == Just (zeroExtend x)
-    it "addresses D" $ property $ \x (Fetch s) ->
-      go s x ^. _1 . osDOp . _1 == s ^. msDPtr
 
   context "ALU" $ do
     let mkalu :: BitVector 13 -> BitVector 16
         mkalu x = 0b011 ++# x
         go s x d r = cycle' s (IS (mkalu x) d r)
+
     it "PC <- I[12] ? R : PC+1" $ property $ \(Fetch s) x d r ->
       go s x d r ^. _2 . msPC ==
         case slice d12 d12 x of
@@ -237,22 +219,54 @@ spec = do
                       go' op s x d r ^. _2 . msT == f s d r
           t'n op f = t' op $ \s n _ -> (s ^. msT) `f` n
 
-      it "T" $ t' 0 $ \s _ _ -> s ^. msT
-      it "N" $ t' 1 $ \_ n _ -> n
-      it "T + N" $ t'n 2 (+)
-      it "T & N" $ t'n 3 (.&.)
-      it "T | N" $ t'n 4 (.|.)
-      it "T | N" $ t'n 5 xor
-      it "~T" $ t' 6 $ \s _ _ -> complement (s ^. msT)
-      it "T == N" $ t'n 7 $ \t n -> pack $ repeat $ t == n
-      it "N < T" $ t'n 8 $
+      it "T"      $ t'   0 $ \s _ _ -> s ^. msT
+      it "N"      $ t'   1 $ \_ n _ -> n
+      it "T + N"  $ t'n  2 (+)
+      it "T & N"  $ t'n  3 (.&.)
+      it "T | N"  $ t'n  4 (.|.)
+      it "T | N"  $ t'n  5 xor
+      it "~T"     $ t'   6 $ \s _ _ -> complement (s ^. msT)
+      it "T == N" $ t'n  7 $ \t n -> pack $ repeat $ t == n
+      it "N < T"  $ t'n  8 $
         \t n -> pack $ repeat $ unpack @(Signed 16) n < unpack t
-      it "N >> T" $ t'n 9 $ \t n -> n `shiftR` fromIntegral t
-      it "T - 1" $ t' 10 $ \s _ _ -> s ^. msT - 1
-      it "R" $ t' 11 $ \_ _ r -> r
+      it "N >> T" $ t'n  9 $ \t n -> n `shiftR` fromIntegral t
+      it "T - 1"  $ t'  10 $ \s _ _ -> s ^. msT - 1
+      it "R"      $ t'  11 $ \_ _ r -> r
       -- 12 reserved
       it "N << T" $ t'n 13 $ \t n -> n `shiftL` fromIntegral t
-      it "depth" $ t' 14 $ \s _ _ -> zeroExtend (s ^. msDPtr)
-      it "N < T" $ t'n 15 $ \t n -> pack $ repeat $ n < t
+      it "depth"  $ t'  14 $ \s _ _ -> zeroExtend (s ^. msDPtr)
+      it "N < T"  $ t'n 15 $ \t n -> pack $ repeat $ n < t
 
+
+instDoesNotWriteM mkinst = it "does not write memory" $ property $
+  \(Fetch s) x -> isNothing $ cycle' s (IS (mkinst x) u u) ^. _1 . osMWrite
+    where u = errorX "must be unused"
+
+instDoesNotWriteR mkinst = it "does not write return" $ property $
+  \(Fetch s) x -> isNothing $ cycle' s (IS (mkinst x) u u) ^. _1 . osROp . _2
+    where u = errorX "must be unused"
+
+instDoesNotWriteD mkinst = it "does not write return" $ property $
+  \(Fetch s) x -> isNothing $ cycle' s (IS (mkinst x) u u) ^. _1 . osDOp . _2
+    where u = errorX "must be unused"
+
+instLeavesLFClear mkinst = it "leaves load flag clear" $ property $
+  \(Fetch s) x -> not $ cycle' s (IS (mkinst x) u u) ^. _2 . msLoadFlag
+    where u = errorX "must be unused"
+
+instChanges mkinst l f = property $
+  \(Fetch s) x -> cycle' s (IS (mkinst x) u u) ^. _2 . l == f (s ^. l)
+    where u = errorX "must be unused"
+
+instPreserves mkinst l = instChanges mkinst l id
+
+instPreservesDPtr mkinst = it "preserves DPtr" $ instPreserves mkinst msDPtr
+instPreservesRPtr mkinst = it "preserves RPtr" $ instPreserves mkinst msRPtr
+
+-- Distinguishes an instruction fetch from a load.
+instFetches mkinst = it "fetches" $ property $
+  \(Fetch s) x ->
+    let (o, s') = cycle' s (IS (mkinst x) u u)
+    in o ^. osMRead == Just (s' ^. msPC)
+    where u = errorX "must be unused"
 
