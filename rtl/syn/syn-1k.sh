846a30a4fed236a42be068a37d61a0fca9468281
Cliff L. Biffle <code@cliffle.com>
2017-11-22 14:54:39 -0800

    Alternate synth support for Icestick/HX1k
    The current demo fits easily in a 1k device, but that's not shocking
    since it does very little.

---
+++ rtl/syn/syn-1k.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+set -e
+
+SYN="$(dirname "$0")"
+ROOT="${SYN}/.."
+OUT="${SYN}/out"
+
+rm -rf "${OUT}"
+
+"${ROOT}/clash" -i"${ROOT}/src" \
+                -outputdir "${OUT}" \
+                --verilog \
+                "${ROOT}/src/CFMTop.hs"
+
+rm -rf "${OUT}"/verilog/CFMTop/*testbench*v
+rm -rf "${OUT}"/verilog/CFMTop/*outputVerifier*v
+
+
+yosys -p "read_verilog ${OUT}/verilog/CFMTop/*.v" \
+      -p "synth_ice40 -top CFMTop_topEntity -abc2 -blif ${OUT}/syn1k.blif"
+
+arachne-pnr -d 1k -p "${SYN}/icestick.pcf" "${OUT}/syn1k.blif" \
+            -o "${OUT}/syn1k.asc"
+
+icepack "${OUT}/syn1k.asc" "${OUT}/syn1k.bin"
+icetime -md hx1k -c 12 "${OUT}/syn1k.asc"
