0df4fc55c6a827e864a3d0d5e10b556923afc476
Cliff L. Biffle <code@cliffle.com>
2017-11-23 16:05:14 -0800

    Str: Factor out shifter.

---
+++ rtl/src/Shifter.hs
@@ -0,0 +1,25 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE ViewPatterns #-}
+
+module Shifter where
+
+import Clash.Prelude hiding (Word)
+
+import Types
+
+leftShift :: Word -> BitVector 4 -> Word
+leftShift input d = revbits $ rightShift (revbits input) d
+  where
+    revbits = pack . reverse . unpack @(Vec _ Bit)
+
+rightShift :: Word -> BitVector 4 -> Word
+rightShift input (unpack -> (s3, s2, s1, s0)) = mux1
+  where
+    mux1 = if s0 then zeroExtend (slice d15 d1 mux2) else mux2
+    mux2 = if s1 then zeroExtend (slice d15 d2 mux4) else mux4
+    mux4 = if s2 then zeroExtend (slice d15 d4 mux8) else mux8
+    mux8 = if s3 then zeroExtend (slice d15 d8 input) else input
--- rtl/src/Str.hs
+++ rtl/src/Str.hs
@@ -8,6 +8,7 @@ module Str where
 
 import Clash.Prelude hiding (Word, v)
 
+import Shifter
 import Types
 
 -- | Combinational datapath for CFM core.
@@ -95,16 +96,3 @@ datapath (MS dptr rptr pc t lf) (IS m n r) =
           , _osROp = (rptr', rop)
           }
      )
-
-leftShift :: Word -> BitVector 4 -> Word
-leftShift input d = revbits $ rightShift (revbits input) d
-  where
-    revbits = pack . reverse . unpack @(Vec _ Bit)
-
-rightShift :: Word -> BitVector 4 -> Word
-rightShift input (unpack -> (s3, s2, s1, s0)) = mux1
-  where
-    mux1 = if s0 then zeroExtend (slice d15 d1 mux2) else mux2
-    mux2 = if s1 then zeroExtend (slice d15 d2 mux4) else mux4
-    mux4 = if s2 then zeroExtend (slice d15 d4 mux8) else mux8
-    mux8 = if s3 then zeroExtend (slice d15 d8 input) else input
