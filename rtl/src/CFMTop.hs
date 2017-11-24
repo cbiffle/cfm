ca113572e846e604d446c6c39c7512c087ddebd4
Cliff L. Biffle <code@cliffle.com>
2017-11-23 16:54:56 -0800

    GPIO output port.

--- rtl/src/CFMTop.hs
+++ rtl/src/CFMTop.hs
@@ -4,14 +4,17 @@
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE BinaryLiterals #-}
+{-# LANGUAGE ViewPatterns #-}
 module CFMTop where
 
 import Clash.Prelude hiding (Word, v, readIO)
 import Control.Lens hiding ((:>))
 import Control.Monad (join)
 import Data.Maybe (fromMaybe)
+import Data.Bool
 import Str
 import Types
+import GPIO
 
 core :: HasClockReset dom gated synchronous
      => Signal dom IS -> Signal dom OS
@@ -20,10 +23,9 @@ core = mealy datapath def
 system :: (HasClockReset dom gated synchronous, KnownNat n, (n + m) ~ 15)
        => Vec (2 ^ n) Word
        -> Signal dom Word
-       -> Signal dom (Maybe (WordAddr, Maybe Word))
-system raminit ioin = ioout
+system raminit = outs
   where
-    coreOuts = core $ IS <$> mux readWasIO ioin mout <*> dout <*> rout
+    coreOuts = core $ IS <$> mux readWasIO ioresp mout <*> dout <*> rout
 
     bread = coreOuts <&> (^. osMRead)
     bwrite = coreOuts <&> (^. osMWrite)
@@ -44,26 +46,36 @@ system raminit ioin = ioout
     repackStack (_, _, Nothing) = Nothing
     repackStack (a, _, Just v) = Just (unpack a, v)
 
+    -- The I/O space is the top 16kiW of the address space, so it's sufficient
+    -- to detect when bit 14 is set. I/O is single ported, so we merge the two
+    -- core ports to create one I/O port; this is correct as long as the core
+    -- doesn't try to execute code from I/O.
+
+    -- Record when I/O was addressed so we can steer the read mux on the
+    -- next cycle.
+    readWasIO = register False $ bread <&> ((/= 0) . slice d14 d14)
+
     writeIO = bwrite <&> maybe False ((/= 0) . slice d14 d14 . fst)
-    readIO = bread <&> slice d14 d14 <&> (/= 0)
-    readWasIO = register False readIO
 
-    packRW _ (Just (a, v)) = (pack a, Just v)
-    packRW a Nothing = (pack a, Nothing)
+    (ioresp, outs) = outport $ iobus
+        <&> \(split -> (m, a), w) -> bool Nothing (Just (a, w)) (unpack m)
+    iobus = iointerface bread bwrite
 
-    ioout = mux ((.|.) <$> readIO <*> writeIO)
-                (Just <$> (packRW <$> bread <*> bwrite))
-                (pure Nothing)
+iointerface :: Signal d WordAddr
+            -> Signal d (Maybe (WordAddr, Word))
+            -> Signal d (WordAddr, Maybe Word)
+iointerface rd wr = repack <$> rd <*> wr
+  where
+    repack _ (Just (a, w)) = (a, Just w)
+    repack a _ = (a, Nothing)
+
 
 topEntity :: Clock System 'Source
           -> Reset System 'Asynchronous
           -> Signal System Word
 topEntity c r = withClockReset @System @'Source @'Asynchronous c r $
   register 0 $
-  system program (pure 0)
-    <&> fmap snd
-    <&> join
-    <&> fromMaybe 0
+  system program
 
 program :: Vec 16 Word
 program =
---
+++ rtl/src/GPIO.hs
@@ -0,0 +1,24 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeOperators #-}
+
+module GPIO where
+
+import Clash.Prelude hiding (Word)
+import Data.Maybe (fromMaybe)
+import Control.Monad (join)
+
+import Types
+
+outport :: (HasClockReset d g s)
+        => Signal d (Maybe (BitVector n, Maybe Word))
+        -> ( Signal d Word
+           , Signal d Word
+           )
+outport cmd = (resp, outs)
+  where
+    outs = register 0 $ fromMaybe <$> outs <*> (join . fmap snd <$> cmd)
+    resp = outs
+
+
