42584a09ab9715386bd4028673612b57cfa22333
Cliff L. Biffle <code@cliffle.com>
2017-11-25 15:40:12 -0800

    tools/test: slow counting to human speeds
    This demonstrates correct operation of
    - A bunch more ALU operations
    - begin ... until
    - ENTER and EXIT

--- tools/test.fs
+++ tools/test.fs
@@ -6,14 +6,25 @@
 0x6103 alu: drop
 0x6600 alu: invert
 0x6203 alu: +
+0x6a03 alu: -
+0x6700 alu: over/=
 
-2 org
+4 org
+\ Delays for u iterations, which in practice means 5u + 3 cycles.
+\ TODO drops to 5u + 2 with drop-return fusion.
+: delay   ( u -- )
+  begin
+    1 -   ( u' )
+    0 over/=
+  until drop ;
+
 : blinky
   0
   begin   ( count )
     0x7FFF invert   ( count ioaddr )
     2dup! drop      ( count )
     1 +   ( count' )
+    1000 delay
   again ;
 
 0 org
