5c0c12219dbc0192fb35af821f125678079ee38e
Cliff L. Biffle <code@cliffle.com>
2017-11-27 09:59:37 -0800

    rtl/src/CFMTop: refactoring

--- rtl/src/CFMTop.hs
+++ rtl/src/CFMTop.hs
@@ -6,46 +6,49 @@
 {-# LANGUAGE BinaryLiterals #-}
 module CFMTop where
 
-import Clash.Prelude hiding (Word, v, readIO)
+import Clash.Prelude hiding (Word, v, readIO, read)
 import Control.Lens hiding ((:>))
+import Data.Maybe (fromMaybe)
 import Str
 import Types
 import IOBus
 import GPIO
 import FlopStack
 
+-- | Registered version of the core datapath.
 core :: HasClockReset dom gated synchronous
      => Signal dom IS -> Signal dom OS
 core = mealy datapath def
 
+-- | Choice of stack implementation technologies.
 data StackType = Flops | RAMs
 
-system :: (HasClockReset dom gated synchronous)
-       => FilePath
-       -> StackType
-       -> Signal dom Word
-system raminit stackType = outs
+-- | Combines 'core' with the selected implementation of stacks, and exposes
+-- the local bus interface.
+coreWithStacks
+  :: (HasClockReset dom gated synchronous)
+  => StackType
+  -> Signal dom Word    -- ^ read response from bus
+  -> ( Signal dom WordAddr
+     , Signal dom (Maybe (WordAddr, Word))
+     ) -- ^ read and write outputs
+coreWithStacks stackType bresp = (bread, bwrite)
   where
-    coreOuts = core $ IS <$> mux readWasIO ioresp mout <*> dout <*> rout
+    coreOuts = core $ IS <$> bresp <*> n <*> r
 
     bread = coreOuts <&> (^. osMRead)
     bwrite = coreOuts <&> (^. osMWrite)
 
-    mread = bread
-    mwrite = bwrite
-
     dop = coreOuts <&> (^. osDOp)
     rop = coreOuts <&> (^. osROp)
 
-    mout = blockRamFile d256 raminit mread (mux writeIO (pure Nothing) mwrite)
-
-    dout = case stackType of
+    n = case stackType of
       Flops -> flopStack d15 (dop <&> (^. _2))
                              (dop <&> (^. _3))
       RAMs  -> blockRamPow2 (repeat 0) (dop <&> (^. _1) <&> unpack)
                                        (dop <&> repackStack)
 
-    rout = case stackType of
+    r = case stackType of
       Flops -> flopStack d16 (rop <&> (^. _2))
                              (rop <&> (^. _3))
       RAMs  -> blockRamPow2 (repeat 0) (rop <&> (^. _1) <&> unpack)
@@ -54,19 +57,46 @@ system raminit stackType = outs
     repackStack (_, _, Nothing) = Nothing
     repackStack (a, _, Just v) = Just (unpack a, v)
 
-    -- The I/O space is the top 16kiW of the address space, so it's sufficient
-    -- to detect when bit 14 is set. I/O is single ported, so we merge the two
-    -- core ports to create one I/O port; this is correct as long as the core
-    -- doesn't try to execute code from I/O.
+-- | Combines 'coreWithStacks' with the requested amount of local-bus RAM and
+-- an I/O bridge, exposing the I/O bus. The RAM is initialized from a file a la
+-- @$readmemb@, which is intended to be used to load a random seed file from
+-- @icebram@.
+coreWithRAM
+  :: (KnownNat ramSize, HasClockReset dom gated synchronous)
+  => StackType          -- ^ Type of stack technology.
+  -> SNat ramSize       -- ^ Size of RAM (need not be pow2).
+  -> FilePath           -- ^ Synthesis-relative RAM initialization file path.
+  -> Signal dom (Maybe Word)  -- ^ I/O read response, valid when addressed.
+  -> ( Signal dom (Maybe (IOAddr, Maybe Word))  -- ^ I/O bus outputs
+     , Signal dom Bool )
+coreWithRAM stackType ramSize ramPath ioresp = (ioreq, readWasIO')
+  where
+    (bread, bwrite) = coreWithStacks stackType bresp
 
-    -- Record when I/O was addressed so we can steer the read mux on the
-    -- next cycle.
-    readWasIO = register False $ bread <&> ((/= 0) . slice d14 d14)
+    -- The I/O response mux remembers whether an I/O read was issued last
+    -- cycle, and uses this information to assert its valid signal. Thus, we
+    -- can use its valid signal to control our mux between memory and I/O.
+    bresp = fromMaybe <$> m <*> ioresp
 
-    writeIO = bwrite <&> maybe False ((/= 0) . slice d14 d14 . fst)
+    m = blockRamFile ramSize ramPath bread $ mux writeIsIO (pure Nothing) bwrite
 
-    (ioresp, outs) = outport iobus
-    iobus = coreToIO bread bwrite
+    -- The I/O bridge generates the ioreq signal and the status signal we use
+    -- to suppress memory writes.
+    (ioreq, readWasIO', writeIsIO) = coreToIO bread bwrite
+
+system :: (HasClockReset dom gated synchronous)
+       => FilePath
+       -> StackType
+       -> Signal dom Word
+system raminit stackType = outs
+  where
+    (ioreq, readWasIO') = coreWithRAM stackType d256 raminit ioresp
+    -- HACK: should use responseMux
+    readWasIO = register False readWasIO'
+
+    -- I/O devices
+    (ioresp', outs) = outport ioreq
+    ioresp = mux readWasIO (Just <$> ioresp') (pure Nothing)
 
 topEntity :: Clock System 'Source
           -> Reset System 'Asynchronous
--- rtl/src/IOBus.hs
+++ rtl/src/IOBus.hs
@@ -4,12 +4,16 @@
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE ViewPatterns #-}
 {-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE FlexibleContexts #-}
 
 -- | IO bus support and interfacing
 module IOBus where
 
-import Clash.Prelude hiding (Word)
+import Clash.Prelude hiding (Word, v, read)
 
+import Control.Monad (join)
+import Control.Arrow (first)
 import Types
 
 -- | Addresses with the top bit set are I/O addresses, leaving 14 bits of space
@@ -19,11 +23,63 @@ type IOAddr = BitVector 14
 -- | Bridges the core's outgoing bus to the narrower I/O bus
 coreToIO :: Signal d WordAddr   -- ^ read address from core
          -> Signal d (Maybe (WordAddr, Word)) -- ^ write command from core
-         -> Signal d (Maybe (IOAddr, Maybe Word))  -- ^ combined I/O interface
-coreToIO = liftA2 datapath
+         -> ( Signal d (Maybe (IOAddr, Maybe Word)) -- ^ combined I/O interface
+            , Signal d Bool                         -- ^ I/O read active signal
+            , Signal d Bool                         -- ^ I/O write active signal
+            )
+coreToIO read write = unbundle $ datapath <$> read <*> write
   where
-    datapath _ (Just (split -> (1, wa), wd)) = Just (wa, Just wd)
-    datapath (split -> (1, ra)) _ = Just (ra, Nothing)
-    datapath _ _ = Nothing
+    datapath _ (Just (split -> (1, wa), wd)) = (Just (wa, Just wd), False, True)
+    datapath (split -> (1, ra)) _ = (Just (ra, Nothing), True, False)
+    datapath _ _ = (Nothing, False, False)
 
 
+-- | Splits an I/O command from the core into device-specific commands, and
+-- provides information for the response multiplexer.
+--
+-- Specifically, this splits an @m + n@ bit address into a device selector (the
+-- top @m@ bits) and a device address (the bottom @n@ bits). The I/O command is
+-- then demultiplexed onto @2 ^ m@ different channels. The index of the channel
+-- chosen is separately provided to be latched by the response mux.
+ioDecoder :: forall n m d. (KnownNat n, KnownNat m)
+          => Signal d (Maybe (BitVector (m + n), Maybe Word))
+          -> ( Vec (2 ^ m) (Signal d (Maybe (BitVector n, Maybe Word)))
+             , Signal d (Maybe (BitVector m))
+             )
+ioDecoder = first unbundle . unbundle . fmap ioDecoder'
+
+ioDecoder' :: forall n m. (KnownNat n, KnownNat m)
+           => Maybe (BitVector (m + n), Maybe Word)
+           -> (Vec (2 ^ m) (Maybe (BitVector n, Maybe Word)), Maybe (BitVector m))
+ioDecoder' input = (map (\i -> join (gate i <$> top <*> input')) indicesI, top)
+  where
+    gate :: Index (2 ^ m) -> BitVector m -> a -> Maybe a
+    gate i b | pack i == b = Just
+             | otherwise = const Nothing
+    input' = truncateAddr input
+    top = topBits . fst <$> input
+
+truncateAddr :: (KnownNat n)
+             => Maybe (BitVector (m + n), Maybe Word)
+             -> Maybe (BitVector n, Maybe Word)
+truncateAddr = fmap $ first truncateB
+
+topBits
+  :: (KnownNat bits, KnownNat extra)
+  => BitVector (bits + extra)
+  -> BitVector bits
+topBits = pack . takeI . unpack @(Vec _ Bit)
+
+-- | Routes device responses.
+--
+-- Each cycle, the 'ioDecoder' sends its muxing decision as a @BitVector m@
+-- (when an I/O device is selected at all. On the next cycle, the 'responseMux'
+-- selects the corresponding channel out of @2 ^ m@ device response channels.
+responseMux :: forall m d g s. (KnownNat m, HasClockReset d g s)
+            => Vec (2 ^ m) (Signal d Word)  -- ^ response from each device
+            -> Signal d (Maybe (BitVector m)) -- ^ decoder output
+            -> Signal d (Maybe Word)  -- ^ response to core
+responseMux inputs ch' = switch <$> bundle inputs <*> ch
+  where
+    ch = register Nothing ch'
+    switch is = fmap (is !!)
