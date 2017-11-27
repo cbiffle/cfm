de6d99c8631ba41fb119437f74266d57095c5f33
Cliff L. Biffle <code@cliffle.com>
2017-11-27 15:51:47 -0800

    rtl/IOBus: improve parametricity

--- rtl/src/IOBus.hs
+++ rtl/src/IOBus.hs
@@ -21,9 +21,9 @@ import Types
 type IOAddr = BitVector 14
 
 -- | Bridges the core's outgoing bus to the narrower I/O bus
-coreToIO :: Signal d WordAddr   -- ^ read address from core
-         -> Signal d (Maybe (WordAddr, Word)) -- ^ write command from core
-         -> ( Signal d (Maybe (IOAddr, Maybe Word)) -- ^ combined I/O interface
+coreToIO :: Signal d WordAddr                       -- ^ read address from core
+         -> Signal d (Maybe (WordAddr, t))          -- ^ write command from core
+         -> ( Signal d (Maybe (IOAddr, Maybe t))    -- ^ combined I/O interface
             , Signal d Bool                         -- ^ I/O read active signal
             , Signal d Bool                         -- ^ I/O write active signal
             )
@@ -41,16 +41,16 @@ coreToIO read write = unbundle $ datapath <$> read <*> write
 -- top @m@ bits) and a device address (the bottom @n@ bits). The I/O command is
 -- then demultiplexed onto @2 ^ m@ different channels. The index of the channel
 -- chosen is separately provided to be latched by the response mux.
-ioDecoder :: forall n m d. (KnownNat n, KnownNat m)
-          => Signal d (Maybe (BitVector (m + n), Maybe Word))
-          -> ( Vec (2 ^ m) (Signal d (Maybe (BitVector n, Maybe Word)))
+ioDecoder :: forall m n t d. (KnownNat m, KnownNat n)
+          => Signal d (Maybe (BitVector (m + n), t))
+          -> ( Vec (2 ^ m) (Signal d (Maybe (BitVector n, t)))
              , Signal d (Maybe (BitVector m))
              )
 ioDecoder = first unbundle . unbundle . fmap ioDecoder'
 
-ioDecoder' :: forall n m. (KnownNat n, KnownNat m)
-           => Maybe (BitVector (m + n), Maybe Word)
-           -> (Vec (2 ^ m) (Maybe (BitVector n, Maybe Word)), Maybe (BitVector m))
+ioDecoder' :: forall m n t. (KnownNat m, KnownNat n)
+           => Maybe (BitVector (m + n), t)
+           -> (Vec (2 ^ m) (Maybe (BitVector n, t)), Maybe (BitVector m))
 ioDecoder' input = (map (\i -> join (gate i <$> top <*> input')) indicesI, top)
   where
     gate :: Index (2 ^ m) -> BitVector m -> a -> Maybe a
@@ -60,8 +60,8 @@ ioDecoder' input = (map (\i -> join (gate i <$> top <*> input')) indicesI, top)
     top = topBits . fst <$> input
 
 truncateAddr :: (KnownNat n)
-             => Maybe (BitVector (m + n), Maybe Word)
-             -> Maybe (BitVector n, Maybe Word)
+             => Maybe (BitVector (m + n), t)
+             -> Maybe (BitVector n, t)
 truncateAddr = fmap $ first truncateB
 
 topBits
@@ -75,10 +75,10 @@ topBits = pack . takeI . unpack @(Vec _ Bit)
 -- Each cycle, the 'ioDecoder' sends its muxing decision as a @BitVector m@
 -- (when an I/O device is selected at all. On the next cycle, the 'responseMux'
 -- selects the corresponding channel out of @2 ^ m@ device response channels.
-responseMux :: forall m d g s. (KnownNat m, HasClockReset d g s)
-            => Vec (2 ^ m) (Signal d Word)  -- ^ response from each device
+responseMux :: forall m t d g s. (KnownNat m, HasClockReset d g s)
+            => Vec (2 ^ m) (Signal d t)  -- ^ response from each device
             -> Signal d (Maybe (BitVector m)) -- ^ decoder output
-            -> Signal d (Maybe Word)  -- ^ response to core
+            -> Signal d (Maybe t)  -- ^ response to core
 responseMux inputs ch' = switch <$> bundle inputs <*> ch
   where
     ch = register Nothing ch'
