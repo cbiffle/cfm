4face87bcf4cba5eeca3eec9a0d38b516b48a6c9
Cliff L. Biffle <code@cliffle.com>
2017-11-29 15:09:37 -0800

    Icestick: wire up DTR and DCD.
    You can now reset the core (not the FPGA) from minicom by requesting a
    hangup (^A H). The core signals back its reset and PLL lock status via
    DCD, which minicom indicates as Online.

--- rtl/syn/icestick-top.v
+++ rtl/syn/icestick-top.v
@@ -6,10 +6,14 @@ module top(
   input clk_12mhz,
   output TX,
   input RX,
+  input dtr_n,
+  output dcd_n,
   output [4:0] led);
 
 wire clk_core;
 wire pll_locked;
+
+wire dtr = ~dtr_n;
 
 `ifndef GO_SLOW
         SB_PLL40_CORE #(
@@ -28,7 +32,7 @@ wire pll_locked;
                 .PLLOUTGLOBAL (clk_core  ),
                 .LOCK         (pll_locked),
                 .BYPASS       (1'b0      ),
-                .RESETB       (1'b1      )
+                .RESETB       (dtr       )
         );
 `else
         assign clk_core = clk_12mhz;
@@ -37,17 +41,25 @@ wire pll_locked;
         reg [3:0] pll_lock_window = 0;
 	reg pll_stable = 0;
 
-	always @(posedge clk_core) begin
+	always @(posedge clk_core or negedge dtr) begin
+          if (~dtr) begin
+              pll_stable <= 0;
+              pll_lock_window <= 0;
+          end else begin
 		pll_stable <= &pll_lock_window;
 		pll_lock_window <= {pll_lock_window, pll_locked};
+          end
 	end
 
         reg [7:0] reset_delay = 0;
-        reg resetn = 0;
+        reg core_reset_n = 0;
 
-        always @(posedge clk_core)
-          if (pll_stable) begin
-            if (reset_delay == 128) resetn <= 1;
+        always @(posedge clk_core or negedge dtr)
+          if (~dtr) begin
+            core_reset_n <= 0;
+            reset_delay <= 0;
+          end else if (pll_stable) begin
+            if (reset_delay == 128) core_reset_n <= 1;
             reset_delay <= reset_delay + 1;
           end
 
@@ -57,14 +69,14 @@ wire pll_locked;
 
         cfm_demo_top _inst(
           .clk_core(clk_core),
-          .reset(~resetn),
+          .reset(~core_reset_n),
           .out1(out1),
           .out2(out2),
           .inport(in),
         );
 
-        assign led = out2[4:0];
+        assign led = {dtr, out2[3:0]};
         assign TX = out1[0];
         assign in = {15'b0, RX};
-
+        assign dcd_n = ~core_reset_n;
 endmodule
