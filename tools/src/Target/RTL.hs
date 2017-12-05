a6f2fe3eee28233d1d9051785007a2ef7e3b6eb2
Cliff L. Biffle <code@cliffle.com>
2017-12-05 14:22:30 -0800

    tools: assemble debug stub instead of hardcoding

--- tools/src/Target/RTL.hs
+++ tools/src/Target/RTL.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE QuasiQuotes #-}
 -- | A target that runs the actual processor RTL in an isolated context.
 module Target.RTL where
 
@@ -11,13 +12,17 @@ import Clash.Sized.BitVector ((++#))
 import qualified Clash.Sized.Vector as V
 import GHC.TypeLits (KnownNat)
 import Data.List (foldl')
+import Data.Either (either)
 import Control.Monad.State.Strict
 import Control.Monad.Reader
 import Control.Concurrent.Chan
 
 import CFM.Types
 import Target
+import Assembler (asmString)
 import qualified RTL.TargetTop as R
+
+import Text.RawString.QQ (r)
 
 -- We'll use a 'Chan' to incrementally create a lazy list of stimuli, and stash
 -- that in the environment as a Reader.
@@ -132,99 +137,100 @@ target img = C.simulateB_lazy $
 
 debugStub :: Vec 8192 Word
 debugStub = V.replace (0 :: Int) 0x1f58 $
-            foldl' (\v (a, i) -> V.replace a i v) (V.repeat 0xDEAD) $
-            zip [0x3E00 `div` 2 :: Int ..]
-                [ 0x6147
-                , 0x700c
-                , 0xfffb
-                , 0x6600
-                , 0x6c00
-                , 0x3f02
-                , 0xfff9
-                , 0x6600
-                , 0x6123
-                , 0x710f
-                , 0xffff
-                , 0x6600
-                , 0x6c00
-                , 0x3f0a
-                , 0xfffd
-                , 0x6600
-                , 0x7c0c
-                , 0x5f0a
-                , 0x8000
-                , 0x6700
-                , 0x3f1c
-                , 0x6103
-                , 0x5f0a
-                , 0x8000
-                , 0x5f02
-                , 0x6c00
-                , 0x5f02
-                , 0x1f11
-                , 0x8001
-                , 0x6700
-                , 0x3f27
-                , 0x6103
-                , 0x5f0a
-                , 0x5f0a
-                , 0x8000
-                , 0x5f02
-                , 0x6123
-                , 0x6103
-                , 0x1f11
-                , 0x8002
-                , 0x6700
-                , 0x3f2f
-                , 0x6103
-                , 0x5f0a
-                , 0x8000
-                , 0x5f02
-                , 0x1f11
-                , 0x8003
-                , 0x6700
-                , 0x3f37
-                , 0x6103
-                , 0x8000
-                , 0x5f02
-                , 0x5f02
-                , 0x1f11
-                , 0x8004
-                , 0x6700
-                , 0x3f40
-                , 0x6103
-                , 0x5f0a
-                , 0x8000
-                , 0x5f02
-                , 0x6147
-                , 0x1f11
-                , 0x8005
-                , 0x6700
-                , 0x3f49
-                , 0x6103
-                , 0x8000
-                , 0x5f02
-                , 0x6b8d
-                , 0x5f02
-                , 0x1f11
-                , 0x8006
-                , 0x6700
-                , 0x3f54
-                , 0x6103
-                , 0x5f0a
-                , 0x8000
-                , 0x5f02
-                , 0x5f00
-                , 0x8000
-                , 0x5f02
-                , 0x1f11
-                , 0x6103
-                , 0x8001
-                , 0x5f02
-                , 0x1f11
-                , 0x8000
-                , 0x8000
-                , 0x6123
-                , 0x6103
-                , 0x1f11
-                ]
+            foldl' (\v (a, i) -> V.replace a i v)
+                   (V.repeat 0xDEAD) $
+            zip [0 :: Int ..] $
+            either error id $
+            asmString $ [r|
+            ( Primitive ALU instruction definitions )
+            0x6180 alu: swap          ( a b -- b a )
+            0x6020 alu: 2dup/!        ( a b -- a b )
+            0x6381 alu: 2dup/and      ( a b -- a b a&b )
+            0x6103 alu: drop          ( x -- )
+            0x6600 alu: invert        ( x -- ~x )
+            0x6203 alu: +             ( a b -- a+b)
+            0x6a03 alu: -             ( a b -- a-b)
+            0x6903 alu: rshift        ( a b -- a>>b )
+            0x6d03 alu: lshift        ( a b -- a<<b )
+            0x6303 alu: and           ( a b -- a&b)
+            0x6403 alu: or            ( a b -- a|b)
+            0x6081 alu: dup           ( x -- x x )
+            0x6c00 alu: @             ( x -- [x] )
+            0x6703 alu: =             ( a b -- a=b )
+            0x6f03 alu: u<            ( a b -- a<b )
+            0x6803 alu: <             ( a b -- a<b )
+            0x6181 alu: over          ( a b -- a b a )
+            0x6e81 alu: depth         ( a b -- a b a )
+            0x6147 alu: >r            ( a --  R: -- a )
+            0x6b8d alu: r>            ( -- a  R: a -- )
+
+            0x3E00 org  ( leave most of RAM available )
+
+            0x8000 constant in-ready?   ( reads non-zero when a word awaits )
+            0x8002 constant in-value    ( reads as last word from host, clears in-ready)
+            0x8004 constant out-ready?  ( reads non-zero when no outgoing word waits )
+            0x8006 constant out-value   ( writes go to host )
+
+            : execute  ( i*x xt -- j*x )  >r ;
+
+            : >host  ( x -- )
+              begin out-ready? @ until  ( spin until FIFO available )
+              out-value ! ;             ( send word )
+
+            : host>  ( -- x )
+              begin in-ready? @ until   ( spin until word available )
+              in-value @ ;              ( receive word )
+
+            : debug-loop
+              host>
+              0 over = if drop  ( peek )
+                host>
+                0 >host   ( command valid )
+                @ >host   ( result of load )
+                debug-loop exit
+              then
+              1 over = if drop  ( poke )
+                host> host>
+                0 >host   ( command valid )
+                !
+                debug-loop exit
+              then
+              2 over = if drop  ( push )
+                host>
+                0 >host
+                debug-loop exit
+              then
+              3 over = if drop  ( pop )
+                0 >host
+                >host
+                debug-loop exit
+              then
+              4 over = if drop  ( >r )
+                host>
+                0 >host
+                >r
+                debug-loop exit
+              then
+              5 over = if drop  ( r> )
+                0 >host
+                r> >host
+                debug-loop exit
+              then
+              6 over = if drop  ( execute )
+                host>
+                0 >host
+                execute
+                0 >host
+                debug-loop exit
+              then
+
+              drop
+              1 >host ( command not recognized)
+              debug-loop ;
+
+            : debug
+              0 0 !   ( clear the reset vector )
+              debug-loop ;
+
+            0 org
+            : reset debug ;|]
