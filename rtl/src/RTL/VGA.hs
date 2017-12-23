5645725c8b4dc923f3414a83a20665e1ebc70c79
Cliff L. Biffle <code@cliffle.com>
2017-12-22 18:34:41 -0800

    VGA: improve commenting

--- rtl/src/RTL/VGA.hs
+++ rtl/src/RTL/VGA.hs
@@ -15,11 +15,45 @@ import Control.Arrow (second)
 import CFM.Types
 
 -------------------------------------------------------------------------------
--- Timing and sync generation
+-- Representation of timing.
 
--- | Timing machine state, either horizontal or vertical. This contains the
--- state with gated updates; the timing control registers, which can be written
--- at any time, are external.
+-- | Phases of a horizontal or vertical timing generator.
+data Phase = FrontPorch -- ^ Start of blanking interval.
+           | SyncPulse  -- ^ Sync pulse.
+           | BackPorch  -- ^ End of blanking interval.
+           | VisibleArea  -- ^ Actual pixels.
+           deriving (Show, Eq, Ord, Enum, Bounded)
+
+-- | Phases advance cyclicly. Note that since they're a 2-bit quantity, we
+-- ought to be able to do this without a carry chain. Not tested.
+nextPhase :: Phase -> Phase
+nextPhase = toEnum . (.&. 3) . (+ 1) . fromEnum
+
+-- | We maintain the number of sub-cycles to spend in each state (minus 1) in a
+-- simple vector, indexed by the 'Phase'. The values themselves are sized by
+-- 'n' (in bits).
+type Timing n = Vec 4 (BitVector n)
+
+-- | Transition function for updating the timing structure itself. This allows
+-- 'Timing' to be exposed as a set of writable registers.
+timingT :: Maybe (BitVector 2, Maybe (BitVector n)) -> Timing n -> Timing n
+timingT (Just (a, Just x)) = replace a x
+timingT _ = id
+
+-- | Here's an example of timing values for a common 800x600 mode.
+vesa800x600x60 :: (Timing 10, Timing 10)
+vesa800x600x60 = ( 39 :> 127 :> 87 :> 799 :> Nil
+                 , 0 :> 3 :> 22 :> 599 :> Nil
+                 )
+
+
+--------------------------------------------------------------------------------
+-- Timing machines and sync generation.
+
+-- | Timing machine state, either horizontal or vertical. This machine is
+-- advanced only when a gate signal allows (e.g. for vertical sync, only at
+-- horizontal blanking). The timing control registers are separate and can be
+-- written at any time.
 data TState n = TState
   { _tsPhase :: Phase
     -- ^ Current phase of the state machine.
@@ -27,55 +61,42 @@ data TState n = TState
     -- ^ Cycles remaining within current phase.
   }
 
-data Phase = FrontPorch
-           | SyncPulse
-           | BackPorch
-           | VisibleArea
-           deriving (Show, Eq, Ord, Enum, Bounded)
-
 makeLenses ''TState
-
--- | Phases advance cyclicly. Note that since they're a 2-bit quantity, we
--- ought to be able to do this without a carry chain. Not tested.
-nextPhase :: Phase -> Phase
-nextPhase = toEnum . (.&. 3) . (+ 1) . fromEnum
 
 instance Default (TState n) where def = TState VisibleArea def
 
--- | We maintain the number of sub-cycles to spend in each state in a simple
--- vector, indexed by the 'Phase'. The values themselves are sized by 'n'.
-type Timing n = Vec 4 (BitVector n)
-
-vesa800x600x60 :: (Timing 10, Timing 10)
-vesa800x600x60 = ( 39 :> 127 :> 87 :> 799 :> Nil
-                 , 0 :> 3 :> 22 :> 599 :> Nil
-                 )
-
-timingT :: Maybe (BitVector 2, Maybe (BitVector n)) -> Timing n -> Timing n
-timingT (Just (a, Just x)) = replace a x
-timingT _ = id
-
 -- | Transition function for updating the state of a timing machine.
+--
+-- The argument order is flipped from our usual Moore machines because of how
+-- it gets used below.
 tstateT :: (KnownNat n)
-        => TState n
-        -> (Bool, Timing n)
+        => (Bool, Timing n) -- ^ Gate and configuration.
         -> TState n
-tstateT s (False, _) = s  -- maintain state when gated
-tstateT s (_, tm) = s & tsPhase .~ phase'
+        -> TState n
+tstateT (False, _) s = s  -- maintain state when gated
+tstateT (_, tm) s = s & tsPhase .~ phase'
                       & tsCycLeft .~ cyc'
   where
     zero = s ^. tsCycLeft == 0
     phase = s ^. tsPhase
     next = nextPhase phase
-    phase' | zero = next
-           | otherwise = phase
-    cyc' | zero = tm !! next
-         | otherwise = pred (s ^. tsCycLeft)
+    -- Advance the phase and reload the counter when the counter expires.
+    (phase', cyc') | zero      = (next,  tm !! next)
+                   | otherwise = (phase, pred (s ^. tsCycLeft))
 
 -- | Timing machine output function.
 --
--- The blanking output signal can be used to produce interrupts, and also to
--- gate the vertical machine from the horizontal.
+-- This produces two "event" outputs and two "level" outputs.
+--
+-- The event outputs are 'True' for exactly one cycle at a specific event:
+--
+-- - Start-of-blank is 'True' during the final cycle of the visible area.
+-- - End-of-blank is 'True' during the final cycle of the blanking interval.
+--
+-- The level outputs are 'True' while a condition holds:
+--
+-- - Sync is 'True' while the sync pulse should be generated.
+-- - Active is 'True' while we're in the visible area of timing.
 tstateO :: (KnownNat n)
         => TState n
         -> ( Bool       -- start of blank
@@ -123,9 +144,19 @@ data GState = GState
 makeLenses ''GState
 
 instance Default GState where
-  def = GState (def, fst vesa800x600x60)
-               (def, snd vesa800x600x60)
-               def def def False False False def def def
+  def = GState
+    { _gsH = (def, fst vesa800x600x60)
+    , _gsV = (def, snd vesa800x600x60)
+    , _gsPixels = def
+    , _gsShadowPixels = def
+    , _gsChar0 = def
+    , _gsHIF = False
+    , _gsVIF = False
+    , _gsEVIF = False
+    , _gsFB = def
+    , _gsAddr = def
+    , _gsReadValue = def
+    }
 
 -- | Mealy function for the framegen circuit.
 --
@@ -145,72 +176,104 @@ framegenT :: GState
 framegenT s iowr = (s', ( (hsync, s ^. gsHIF)
                         , (vsync, s ^. gsVIF, s ^. gsEVIF)
                         , act
-                        , vid
+                        , s ^. gsPixels
                         , s ^. gsFB
                         , write
                         , s ^. gsReadValue
                         ))
   where
-    s' = s & gsH . _1 %~ flip tstateT (True, s ^. gsH . _2)
-           & gsH . _2 %~ timingT hwr
-           & gsV . _1 %~ flip tstateT (hblank, s ^. gsV . _2)
-           & gsV . _2 %~ timingT vwr
-           & gsPixels .~ pixels'
+    s' = s & gsPixels .~ pixels'
            & gsShadowPixels .~ shadowPixels'
            & gsChar0 .~ char0'
-           & gsHIF %~ ((&& not hack) . (|| hblank))
-           & gsVIF %~ ((&& not vack) . (|| vblank))
-           & gsEVIF %~ ((&& not evack) . (|| evblank))
            & gsFB .~ fb'
            & gsAddr .~ addr'
            & gsReadValue .~ readValue
+           -- Timing machine updates
+           & gsH . _1 %~ tstateT (True, s ^. gsH . _2)
+           & gsV . _1 %~ tstateT (hblank, s ^. gsV . _2)
+           -- Timing register updates
+           & gsH . _2 %~ timingT hwr
+           & gsV . _2 %~ timingT vwr
+           -- Interrupt flags: set on event, clear on acknowledge
+           & gsHIF  %~ ((&& not hack)  . (|| hblank))
+           & gsVIF  %~ ((&& not vack)  . (|| vblank))
+           & gsEVIF %~ ((&& not evack) . (|| evblank))
 
-    iosplit (Just (split -> (t, a), x)) = case t of
-      0 -> (Just (a, truncateB <$> x), Nothing, Nothing)
-      1 -> (Nothing, Just (a, truncateB <$> x), Nothing)
-      _ -> (Nothing, Nothing, Just (t ++# a, x))
-    iosplit _ = (Nothing, Nothing, Nothing)
+    -- Timing machine output circuits.
+    (hblank, _,       hsync, hactive) = tstateO (s ^. gsH . _1)
+    (vblank, evblank, vsync, vactive) = tstateO (s ^. gsV . _1)
+    act = hactive && vactive
 
-    (hwr, vwr, rwr) = iosplit iowr
+    -- We expose three separate sets of registers to the bus: the horizontal and
+    -- vertical timing, and our own control registers. Split requests here.
+    -- TODO: this is a recurring pattern that could be better extracted.
+    (hwr, vwr, rwr) = iowr & \w -> case w of
+      Just (split -> (t, a), x)
+        | t == 0    -> (Just (a, truncateB <$> x), Nothing, Nothing)
+        | t == 1    -> (Nothing, Just (a, truncateB <$> x), Nothing)
+        | otherwise -> (Nothing, Nothing, Just (t ++# a, x))
+      _ -> (Nothing, Nothing, Nothing)
 
+    -- The start-of-field event occurs at the start-of-hblank cycle during the
+    -- last line of the vertical blanking interval, and is used to reset state
+    -- for the coming visible field.
     startOfField = hblank && evblank
+
     lastGlyphSlice = s ^. gsFB == 7 -- TODO programmable
 
+    -- Transition rules for the pixel addressing counter.
     -- TODO all these equations need optimizin'.
     pixels' | Just (0x8, Just v) <- rwr = truncateB (v `shiftL` 3)
+              -- Allow host writes at any time.
             | startOfField = (s ^. gsChar0) ++# 0
+              -- Reset to char0 at start-of-field.
             | vactive && hblank && not lastGlyphSlice = s ^. gsShadowPixels
+              -- For all rows of a glyph save the last, reset to the shadow
+              -- at hblank.
             | hactive && vactive = s ^. gsPixels + 1
+              -- Increment when active (includes final hblank of glyph).
             | otherwise = s ^. gsPixels
 
+    -- Transition rules for the shadow pixel addressing counter.
     shadowPixels' | vactive && hblank && lastGlyphSlice = s ^. gsPixels + 1
+                    -- Take new value at hblank on final row of glyph.
                   | startOfField = (s ^. gsChar0) ++# 0
+                    -- Reset to char0 at start-of-field.
                   | otherwise = s ^. gsShadowPixels
 
+    -- Interrupt acknowledge signals, detected when the host writes 1 to bits in
+    -- register 0x12.
     (evack, hack, vack) | Just (0x9, Just v) <- rwr = unpack (slice d2 d0 v)
                         | otherwise = (False, False, False)
 
+    -- Font base transition rules.
     fb' | Just (0xA, Just v) <- rwr = truncateB v
+          -- Allow host writes at any time.
         | startOfField = 0
+          -- Reset to zero at start-of-field. This ensures correct display when
+          -- the visible area isn't an integral number of glyph rows.
         | hblank = s ^. gsFB + 1
+          -- Advance at hblank. This assumes rollover is desirable.
+          -- TODO: programmable glyph heights will complicate this.
         | otherwise = s ^. gsFB
 
-    (hblank, _, hsync, hactive) = tstateO (s ^. gsH . _1)
-    (vblank, evblank, vsync, vactive) = tstateO (s ^. gsV . _1)
-    vid = s ^. gsPixels
-    act = hactive && vactive
-
+    -- Video memory write address transition rules.
     addr' | Just (0xB, Just v) <- rwr = unpack $ truncateB v
-          | Just _ <- write = second (+1) $ s ^. gsAddr -- autoincrement on use
+            -- Allow host writes at any time.
+          | Just _ <- write = second (+1) $ s ^. gsAddr
+            -- On any use of VWD (below), advance the address.
           | otherwise = s ^. gsAddr
 
+    -- Detecting writes to VWD.
     write
       | Just (0xC, Just v) <- rwr = Just (s ^. gsAddr, truncateB v)
       | otherwise = Nothing
 
+    -- Transition rules for char0, a simple register.
     char0' | Just (0xD, Just v) <- rwr = truncateB v
-          | otherwise = s ^. gsChar0
+           | otherwise = s ^. gsChar0
 
+    -- Bus response multiplexer.
     readValue = case fromMaybe 0 (fst <$> iowr) of
           x | x .&. 0xC == 0 ->
                 zeroExtend $ (s ^. gsH . _2) !! (x .&. 3)
